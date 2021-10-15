69ee6cbed02cda34547c80de55026d524eaddd8b
Nhahan <kisy324@naver.com>
2021-10-15 23:43:57 +0900

    localstorage USERNAME_KEY

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting")
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username"
 
 function onLoginSubmit(event) {
     event.preventDefault();
@@ -10,9 +11,19 @@ function onLoginSubmit(event) {
     loginForm.classList.add("hidden");
 
     console.log(username);
-    
+
+    localStorage.setItem(USERNAME_KEY, username);
+
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit)
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    // show the form
+} else {
+    // show the greetings
+}
