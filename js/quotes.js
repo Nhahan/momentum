7112ad6a245af09021291e4bc7e985b14093d50f
Nhahan <kisy324@naver.com>
2021-10-17 18:33:46 +0900

    quotes initialize

---
+++ js/quotes.js
@@ -0,0 +1,91 @@
+const quotes = [{
+    quote: "Your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma – which is living with the results of other people’s thinking. Don’t let the noise of other’s opinions drown out your own inner voice. And most important, have the courage to follow your heart and intuition. They somehow already know what you truly want to become. Everything else is secondary.",
+    author: "Steve Jobs, Co-founder, CEO, Chairman Apple Inc.",
+}, {
+    quote: "My biggest motivation? Just to keep challenging myself. I see life almost like one long University education that I never had — every day I’m learning something new.",
+    author: "Richard Branson, founder Virgin Group"
+}, {
+    quote: "Every time you state what you want or believe, you’re the first to hear it. It’s a message to both you and others about what you think is possible. Don’t put a ceiling on yourself.",
+    author: "Oprah Winfrey, media proprietor",
+}, {
+    quote: "Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do.",
+    author: "Steve Jobs, Co-founder, CEO, Chairman Apple Inc.",
+}, {
+    quote: "I knew that if I failed I wouldn’t regret that, but I knew the one thing I might regret is not trying.",
+    author: "Jeff Bezos, founder and CEO Amazon",
+}, {
+    quote: "Whether you think you can, or think you can’t — you’re right.",
+    author: "Henry Ford, Founder Ford Motor Company",
+}, {
+    quote: "Don’t limit yourself. Many people limit themselves to what they think they can do. You can go as far as your mind lets you. What you believe, remember, you can achieve.",
+    author: "Mary Kay Ash, Founder Mary Kay Cosmetics"
+}, {
+    quote: "You don’t learn to walk by following rules. You learn by doing and falling over.",
+    author: "Richard Branson, founder Virgin Group",
+}, {
+    quote: "When you find an idea that you just can’t stop thinking about, that’s probably a good one to pursue.",
+    author: "Josh James, co-founder and CEO Omniture, founder and CEO Domo",
+}, {
+    quote: "It’s not about ideas. It’s about making ideas happen.",
+    author: "Scott Belsky, co-founder Behance"
+}, {
+    quote: "Entrepreneur is someone who has a vision for something and a want to create.",
+    author: "David Karp, founder and CEO Tumblr",
+}, {
+    quote: "The only thing worse than starting something and failing… is not starting something.",
+    author: "Seth Godin, founder Squidoo"
+}, {
+    quote: "When I’m old and dying, I plan to look back on my life and say ‘wow, that was an adventure,’ not ‘wow, I sure felt safe.’",
+    author: "Tom Preston-Werner, co-founder Github",
+}, {
+    quote: "The fastest way to change yourself is to hang out with people who are already the way you want to be.",
+    author: "Reid Hoffman, co-founder LinkedIn",
+}, {
+    quote: "I don’t look to jump over 7-foot bars — I look for 1-foot bars that I can step over.",
+    author: "Warren Buffett, chairman and CEO Berkshire Hathaway",
+}, {
+    quote: "In the end, a vision without the ability to execute it is probably a hallucination.",
+    author: "Steve Case, co-founder AOL",
+}, {
+    quote: "Fearlessness is like a muscle. I know from my own life that the more I exercise it the more natural it becomes to not let my fears run me.",
+    author: "Arianna Huffington, president and editor in chief The Huffington Post Media Group"
+}, {
+    quote: "Embrace what you don’t know, especially in the beginning, because what you don’t know can become your greatest asset. It ensures that you will absolutely be doing things different from everybody else.",
+    author: "Sara Blakely, founder SPANX",
+}, {
+    quote: "Risk more than others think is safe. Dream more than others think is practical.",
+    author: "Howard Schultz, Starbucks CEO",
+}, {
+    quote: "The way to get started is to quit talking and begin doing.",
+    author: "Walt Disney, founder Disney",
+}, {
+    quote: "High expectations are the key to everything.",
+    author: "Sam Walton, founder Walmart",
+}, {
+    quote: "Don’t be afraid to assert yourself, have confidence in your abilities and don’t let the bastards get you down.",
+    author: "Michael Bloomberg, founder Bloomberg L.P."
+}, {
+    quote: "There are lots of bad reasons to start a company. But there’s only one good, legitimate reason, and I think you know what it is: it’s to change the world.",
+    author: "Phil Libin, CEO Evernote",
+}, {
+    quote: "Everything started as nothing.",
+    author: "Ben Weissenstein, founder and CEO The Entitled Group",
+}, {
+    quote: "The important thing is not being afraid to take a chance. Remember, the greatest failure is to not try. Once you find something you love to do, be the best at doing it.",
+    author: "Debbi Fields, found Mrs. Fields Cookies"
+}, {
+    quotes: "Empower yourself and realize the importance of contributing to the world by living your talent. Work on what you love. You are responsible for the talent that has been entrusted to you.",
+    author: "Catharina Bruns, Founder WorkIsNotaJob",
+}, {
+    quotes: "The unexamined life is not worth living",
+    author: "Socrates",
+}, {
+    quotes: "Whereof one cannot speak, thereof one must be silent",
+    author: "Ludwig Wittgenstein",
+}, {
+    quotes: "Entities should not be multiplied unnecessarily",
+    author: "William of Ockham",
+}, {
+    quotes: "The life of man (in a state of nature) is solitary, poor, nasty, brutish, and short",
+    author: "Thomas Hobbes",
+}];
