59b064e2fbd876643b20232085d10f61b39a92ea
Nhahan <kisy324@naver.com>
2021-10-18 23:40:13 +0900

    weather set

--- js/weather.js
+++ js/weather.js
@@ -4,14 +4,19 @@ function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
 
-    const url = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    const url = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
 
-    fetch(url).then(r => r.json()).then(d => {
-        const weather = document.querySelector("#weather span:first-child")
-        const city = document.querySelector("#weather span:last-child")
-        weather.innerText = d.weather[0].main
-        city.innerText = d.name;
-    });
+    fetch(url)
+        .then(r => r.json())
+        .then(d => {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+
+            console.log(weather)
+
+            weather.innerText = d.weather[0].main;
+            city.innerText = d.name;
+        });
 }
 
 function onGeoError() {
